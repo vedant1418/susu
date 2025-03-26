ce8dac95c1f21d82c7b66418751d6d7ea0ccf861
vedant1418 <108506814+vedant1418@users.noreply.github.com>
2025-03-26 04:36:36 +0000

    Initial commit

--- Ashok-Vatika-main/ashokvatika/urls.py
+++ Ashok-Vatika-main/ashokvatika/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# /workspaces/susu/Ashok-Vatika-main/ashokvatika/urls.py
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin URL
     path('admin/', admin.site.urls),
-    path('', include('ashokapp.url')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Main app URL inclusion
+    path('', include('ashokapp.urls')),  # Ensure 'ashokapp.urls' is correctly defined
+
+] 
+
+# Serving media files during development (when DEBUG=True)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
